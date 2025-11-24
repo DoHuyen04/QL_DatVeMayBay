a3b823ad585dfbde3be7e8c60edec1771aceb075
MeiiiHuonggg <huogmai143@gmail.com>
2025-11-25 00:16:46 +0700

    Sửa giao diện TrangChu (chưa chèn ảnh)

--- QLDatVeMayBay/obj/Debug/net9.0/QLDatVeMayBay.AssemblyInfo.cs
+++ QLDatVeMayBay/obj/Debug/net9.0/QLDatVeMayBay.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QLDatVeMayBay")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+296d703a3c4b24f82f133ee1c00a3136c9193dc8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0495e76c62394e061e7c195a5836305b40464e35")]
 [assembly: System.Reflection.AssemblyProductAttribute("QLDatVeMayBay")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QLDatVeMayBay")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
